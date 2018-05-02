51ce3ba0dada0a0f681a7b59330596e244cb6ea2
djlowes <djlowes@gmail.com>
2018-05-02 11:51:33 -0700

    heroku test

--- trails.js
+++ trails.js
@@ -1,10 +1,10 @@
-const dotenv = require('dotenv');
-const dotenvParseVariables = require('dotenv-parse-variables');
-
-let env = dotenv.config({})
-if (env.error) throw env.error;
-env = dotenvParseVariables(env.parsed);
-console.log(env);
+// const dotenv = require('dotenv');
+// const dotenvParseVariables = require('dotenv-parse-variables');
+//
+// let env = dotenv.config({})
+// if (env.error) throw env.error;
+// env = dotenvParseVariables(env.parsed);
+// console.log(env);
 
 
 const twilio = require('twilio')(
